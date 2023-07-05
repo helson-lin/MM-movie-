93d67462ae23ff676d6dbeef43e36589e018bce4
helson-lin <84565447+helson-lin@users.noreply.github.com>
2023-07-05 04:45:52 +0000

    feat: add source

--- public/config.js
+++ public/config.js
@@ -2,7 +2,8 @@ window.SOURCES = {
     '高清': '/gaoqing/',
     '百度': '/baidu/',
     '天空': '/tiankong/',
-    '无尽': '/wujin/'
+    '无尽': '/wujin/',
+    'iKun': '/ikun/'
     // '天空': 'https://m3u8.tiankongapi.com/api.php/provide/vod/',
     // '百度': 'https://api.apibdzy.com/api.php/provide/vod/'
 }
